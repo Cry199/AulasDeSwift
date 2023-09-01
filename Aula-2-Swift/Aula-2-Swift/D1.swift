bb1427401da8bc8c8e09d6f33f3a286906e68942
Cry199 <caualeu2016@gmail.com>
2023-09-01 16:47:46 -0300

    v0.0.2 - Aula 2 IMC Pronto

---
+++ Aula-2-Swift/Aula-2-Swift/D1.swift
@@ -0,0 +1,20 @@
+//
+//  D1.swift
+//  Aula-2-Swift
+//
+//  Created by Student02 on 01/09/23.
+//
+
+import SwiftUI
+
+struct D1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct D1_Previews: PreviewProvider {
+    static var previews: some View {
+        D1()
+    }
+}
